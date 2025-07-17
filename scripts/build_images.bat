1ee84ccaae3ffaf5e54f94e59b5538de50ed725b
Biruk Belihu <birukbelihu1995@gmail.com>
2025-07-17 09:53:51 +0300

    Fixed scripts path issues

--- scripts/build_images.bat
+++ scripts/build_images.bat
@@ -1,17 +1,17 @@
 @echo off
 echo 🚀 Building Docker images for Cody...
 
-docker build -t code_runner_python images\python
+docker build -t code_runner_python ..\images\python
 if errorlevel 1 (
     echo ❌ Failed to build Python image
     exit /b 1
 )
 
-docker build -t code_runner_javascript images\javascript
+docker build -t code_runner_javascript ..\images\javascript
 if errorlevel 1 (
     echo ❌ Failed to build JavaScript(Node.js) image
     exit /b 1
 )
 
-echo ✅ All docker images built successfully.
+echo ✅ All docker images built successfully!!!
 pause
--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,7 +2,7 @@
 
 echo "🚀 Building Docker images for Cody..."
 
-docker build -t code_runner_python images/python || { echo "❌ Failed to build Python image"; exit 1; }
-docker build -t code_runner_javascript images/javascript || { echo "❌ Failed to build JavaScript(Node.js) image"; exit 1; }
+docker build -t code_runner_python ../images/python || { echo "❌ Failed to build Python docker image."; exit 1; }
+docker build -t code_runner_javascript ../images/javascript || { echo "❌ Failed to build JavaScript(Node.js) docker image."; exit 1; }
 
-echo "✅ All docker images built successfully."
+echo "✅ All docker images built successfully!!!"
